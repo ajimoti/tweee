64f50b1d829a7ac689520ea7fccb05ca15d4568b
ajibukunoluwa <ibukunajmoti@gmail.com>
2024-09-03 14:19:00 +0100

    several updates

--- twitter_bot_project/settings.py
+++ twitter_bot_project/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'twitter_bot_project.middleware.SlackExceptionMiddleware',
 ]
 
 ROOT_URLCONF = "twitter_bot_project.urls"
